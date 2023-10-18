a3c3e371eedcebe13725df85d98b11e78416c43e
Hafizzargar <96607751+Hafizzargar@users.noreply.github.com>
2023-10-18 15:09:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,8 +2,17 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
+	let en=Object.entries(sampleObject);
+	for(let i=0;i<en.length;i++){
+		if(en[i][0]===key){
+			return true;
+		}
+	}
+	return false;
+	
+	
 }
 
 // Do not change the code below
-const key = prompt("Enter Key.");
+const key = prompt("Enter Key.")
 alert(hasKey(key));
